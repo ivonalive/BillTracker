8fc911e26cc36c9e90cdb3f84947118de99dc895
Ivona Olvera-Radenovic <ivonaolvera@gmail.com>
2024-08-18 20:25:09 -0700

    Changed from TypeScript to JS

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
 
 import "./Userpage.css";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
@@ -10,50 +11,42 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
 
+function UserPage() {
+  
+  const [events, setEvents] = React.useState([]);
 
-// const AllViews = {
-//   month: true,
-//   week: true,
-//   day: true,
-//   date: CustomDateView,
-// }
-
-class UserPage extends Component {
-  state = {
-    events: [],
-  };
-
-  onEventResize = (data) => {
+  const onEventResize = (data) => {
     const { start, end } = data;
-
-    this.setState((state) => {
-      state.events[0].start = start;
-      state.events[0].end = end;
-      return { events: [...state.events] };
+    setEvents((prevEvents) => {
+      const updatedEvents = [...prevEvents];
+      updatedEvents[0].start = start;
+      updatedEvents[0].end = end;
+      return updatedEvents;
     });
   };
-
-  onEventDrop = (data) => {
+  const onEventDrop = (data) => {
     console.log(data);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <DnDCalendar
-          defaultDate={moment().toDate()}
-          defaultView="month"
-          // views={AllViews}
-          events={this.state.events}
-          localizer={localizer}
-          onEventDrop={this.onEventDrop}
-          onEventResize={this.onEventResize}
-          resizable
-          style={{ height: "100vh" }}
-        />
-      </div>
-    );
-  }
+
+  return (
+    <div className="App">
+      <DnDCalendar
+        defaultDate={moment().toDate()}
+        defaultView="month"
+        events={events}
+        max={moment("23:59:00", "HH:mm:ss").toDate()}
+        min={moment("00:00:00", "HH:mm:ss").toDate()}
+        step={60}
+        timeslots={1}
+        localizer={localizer}
+        onEventDrop={onEventDrop}
+        onEventResize={onEventResize}
+        resizable
+        style={{ height: "100vh" }}
+      />
+    </div>
+  );
 }
 
 export default UserPage;
