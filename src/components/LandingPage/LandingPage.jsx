810593d1b6b4c6f0dcdeb101536cdcb840c2a6d5
Ivona Olvera-Radenovic <ivonaolvera@gmail.com>
2024-10-12 08:49:04 -0700

    Welcome heading moved to center of paragraph

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -15,10 +15,11 @@ function LandingPage() {
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
 
       <div className="grid">
         <div className="grid-col grid-col_8">
+      <h2>{heading}</h2>
+
           <p>
           BillTracker allows users to prevent late fee charges. Some of them may be car insurance, healthcare coverage, streaming services, cellular services, rent, credit card payments etc. Instead of remembering all bills due a month the user can use this application as note taking for bills. 
 
