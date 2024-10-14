11cd50a019346fb3ab23c6a4184fbd3eab124104
Ivona Olvera-Radenovic <ivonaolvera@gmail.com>
2024-10-13 21:27:58 -0700

    landing page adjusted to mobile view and input fields styled

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -20,24 +20,26 @@ function LandingPage() {
       <div className="grid">
         <div className="grid-col grid-col_8">
       <h2>{heading}</h2>
-
+      <div id="welcome-text">
           <p>
           BillTracker allows users to prevent late fee charges. Some of them may be car insurance, healthcare coverage, streaming services, cellular services, rent, credit card payments etc. Instead of remembering all bills due a month the user can use this application as note taking for bills. 
-
           </p>
-
           <p>
           To help relieve stress BillTracker allows users to take a look at their upcoming bills and plan their finances accordingly.
           </p>
+      </div>
         </div>
         <div className="grid-col grid-col_4">
+        <div id="register-form">
           <RegisterForm />
-
+        </div>
           <center>
+        <div id="login-section">
             <h4>Already a Member?</h4>
             <button className="btn btn_sizeSm" onClick={onLogin}>
               Login
             </button>
+        </div>  
           </center>
         </div>
       </div>
